a03c0b11f9c70c7818915bc3a87d2101b1205dc5
Bankz001 <kunatipe@gmail.com>
2025-01-25 10:03:16 +0700

    f1

--- wordle/Wordle.py
+++ wordle/Wordle.py
@@ -1,1 +1,14 @@
+import os
+import random
+
+def wordlist():
+    path = './wordle/valid-wordle-words.txt'
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            word = f.read()
+            print(random.choice(word))
+            
+        print(f"File not found: {path}")
+
+wordlist()
 
