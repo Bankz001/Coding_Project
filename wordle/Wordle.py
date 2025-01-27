fd8bf2c6e2ce117c6c8c6648cbfdf09e7fc7bc23
Bankz001 <kunatipe@gmail.com>
2025-01-27 15:23:53 +0700

    answering function

--- wordle/Wordle.py
+++ wordle/Wordle.py
@@ -5,11 +5,9 @@ def txt_to_wordlist(file_path):
         # Open the file
         with open(file_path, 'r') as file:
             # Read the content of the file
-            content = file.read()
-            
+            content = file.read()           
             # Split the content into words (split by whitespace)
-            word_list = content.split()
-            
+            word_list = content.split()    
         return word_list
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
@@ -18,16 +16,30 @@ def txt_to_wordlist(file_path):
         print(f"An error occurred: {e}")
         return []
 
-# Example usage
+def answering():
+    answer = input("Enter a 5 letters word : ")
+    while len(answer) != 5:
+        print("Invalid answer")
+        answer = input("Enter a 5 letters word : ")
+    return answer    
+
 file_path = './wordle/valid-wordle-words.txt'  # Replace with your file path
 word_list = txt_to_wordlist(file_path)
-
 word = random.choice(word_list)
-answer = input("Enter a 5 letters word : ")
-
-if answer == word :
-    print("correct")
-else : 
-    print("not correct")
 
 print(word)
+print("WORDLE")
+print("You have 5 chances to answer the correct answer")
+
+i = 5
+answer = answering()
+while answer != word and i>1:
+    i-=1
+    if answer != word :
+        print("You have", i,"chances left.")
+    answer = answering()  
+    
+if answer == word :
+    print("Congratuation")
+else:
+    print("The correct answer is:", word)
