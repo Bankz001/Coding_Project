abd63a3d2a409c3b508429cfa8ac8d1f0009df87
Bankz001 <kunatipe@gmail.com>
2025-01-31 13:45:30 +0700

    color coding the answer

--- wordle/Wordle.py
+++ wordle/Wordle.py
@@ -1,5 +1,10 @@
 import os
 import random
+from colorama import init
+from termcolor import colored
+
+init()
+
 def txt_to_wordlist(file_path):
     try:
         # Open the file
@@ -16,31 +21,50 @@ def txt_to_wordlist(file_path):
         print(f"An error occurred: {e}")
         return []
 
-def answering():
+def answering(word):
     ans = input("Enter a 5 letters word : ")
     answer = ans.lower()
     while len(answer) != 5:
         print("Invalid answer")
         answer = input("Enter a 5 letters word : ")
+    print('-',end='')
+    answer_l = list(answer)
+    word_l = list(word)
+    for i in range(len(answer_l)):
+        c1 = False
+        c2 = False
+        for j in range(len(word_l)):
+            if answer_l[i] == word_l[j]:
+                c1 = True
+                if i==j :
+                    c2 = True
+        if c1 == True and c2 == True:
+            print(colored(answer_l[i],'green'),end='')    
+        elif c1 == True and c2 == False:
+            print(colored(answer_l[i],'yellow'),end='')  
+        else:
+            print(colored(answer_l[i],'red'),end='')
+    print()
     return answer    
 
 file_path = './wordle/valid-wordle-words.txt'  # Replace with your file path
 word_list = txt_to_wordlist(file_path)
 word = random.choice(word_list)
 
-#print(word)
+print(word)
 print("WORDLE")
 print("You have 5 chances to answer the correct answer")
 
 i = 5
-answer = answering()
+answer = answering(word)
 while answer != word and i>1:
     i-=1
     if answer != word :
         print("You have", i,"chances left.")
-    answer = answering()  
+    answer = answering(word)  
     
 if answer == word :
-    print("Congratuation")
+    print(colored("Congratuation",'green'))
 else:
     print("The correct answer is:", word)
+    
